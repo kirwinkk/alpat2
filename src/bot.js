99ba144755929a4215418861a64aefe8daf59a50
ervan0707 <32308373+ervan0707@users.noreply.github.com>
2017-10-17 01:40:00 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,11 @@ const LineConnect = require('./connect');
 let line = require('./main.js');
 let LINE = new line();
 
- const auth = {
- 	authToken: 'El1S6foZOlYbmDwnQ5X3.VgoedwlLOECsbTU8wBy6uW.5gZlmh1YDYn+tQevNQ4krgaswU8Osz7IpwZBFsv2BkU=',
- }
- let client =  new LineConnect(auth);
-//let client =  new LineConnect();
+ //const auth = {
+ //	authToken: 'El1S6foZOlYbmDwnQ5X3.VgoedwlLOECsbTU8wBy6uW.5gZlmh1YDYn+tQevNQ4krgaswU8Osz7IpwZBFsv2BkU=',
+ //}
+// let client =  new LineConnect(auth);
+let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
